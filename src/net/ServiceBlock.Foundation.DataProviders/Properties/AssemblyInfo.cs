f458b0a14cdb071a9125d8bfc213d7368cace44b
Software Rockstar <mharoon@softwarerockstar.com>
2014-10-30 16:12:20 -0500

    Updated version to 0.0.19.0-beta. Publishe to NuGet

--- src/net/ServiceBlock.Foundation.DataProviders/Properties/AssemblyInfo.cs
+++ src/net/ServiceBlock.Foundation.DataProviders/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ServiceBlock")]
 [assembly: AssemblyProduct("ServiceBlock.Foundation.DataProviders")]
-[assembly: AssemblyCopyright("Copyright © ServiceBlock.Foundation, 2014")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.20.0")]
-[assembly: AssemblyFileVersion("2.0.20.0")]
+[assembly: AssemblyVersion("0.0.19.0")]
+[assembly: AssemblyFileVersion("0.0.19.0")]
